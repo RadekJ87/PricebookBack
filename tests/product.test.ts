1d76fdc89044c1633be3ef27f5ab6088f9c4fca5
Radek Janiszewski <janiszewski.radoslaw87@gmail.com>
2022-07-09 10:04:50 +0200

    one path to get all products - with optionable search

--- tests/product.test.ts
+++ tests/product.test.ts
@@ -1,41 +1,51 @@
 import {ProductRecord} from "../records/product.record";
-import { pool } from "../utils/db";
+import {pool} from "../utils/db";
 
+
+const basicObject = {
+    description: 'Welded table',
+    drawingNumber: '000456',
+    revision: '0',
+    itemNumber: '',
+    moq: 5,
+    price: 123.99,
+    offerNumber: ''
+};
 
 afterAll(async () => {
     await pool.end();
 });
 
 
-test('ProductRecord returns data for one entry', async ()=>{
+test('ProductRecord returns data for one entry', async () => {
     const product = await ProductRecord.getOne('2');
 
     expect(product).toBeDefined();
     expect(product.description).toBe('Biurko');
 });
 
-test('ProductRecord returns null for unexisting entry', async ()=>{
+test('ProductRecord returns null for unexisting entry', async () => {
     const product = await ProductRecord.getOne('10');
 
     expect(product).toBeNull();
 });
 
 
-test('ProductRecord finds data for one entry', async ()=>{
+test('ProductRecord finds data for one entry', async () => {
     const product = await ProductRecord.findOne('456');
 
-    console.log(product);
+
     expect(product).toBeDefined();
     expect(product.drawingNumber).toBe('456');
 });
 
-test('ProductRecord cannot find data one entry', async ()=>{
+test('ProductRecord cannot find data one entry', async () => {
     const product = await ProductRecord.findOne('45');
 
     expect(product).toBeNull();
 });
 
-test('ProductRecord returns array of found entries', async ()=>{
+test('ProductRecord returns array of found entries', async () => {
     const products = await ProductRecord.findAll('');
 
 
@@ -43,29 +53,35 @@ test('ProductRecord returns array of found entries', async ()=>{
     expect(products[0].moq).toBeGreaterThanOrEqual(6);
 });
 
-test('ProductRecord returns array of products for search of drawings containing products phrase `25`', async ()=>{
+test('ProductRecord returns array of products for search of drawings containing products phrase `25`', async () => {
     const products = await ProductRecord.findAll('25');
 
-    console.log(products);
     expect(products).toBeDefined();
     expect(products).not.toBe([]);
-    expect(products.length). toBe(2);
+    expect(products.length).toBe(2);
     expect(products[0].description).toBe('Komoda');
 });
 
-test('ProductRecord returns empty array for search of data that not exist', async ()=>{
+test('ProductRecord returns empty array for search of data that not exist', async () => {
     const products = await ProductRecord.findAll('agsd');
 
     expect(products).toBeDefined();
     expect(products).toEqual([]);
 });
 
-test('ProductRecord returns data of all found entries', async ()=>{
+test('ProductRecord returns data of all found entries', async () => {
     const products = await ProductRecord.getAll();
 
-    console.log(products);
     expect(products).toBeDefined();
     expect(products).not.toBe([]);
     expect(products.length).toBeGreaterThanOrEqual(5);
 });
+
+test('ProductRecord insterts data to DB', async () => {
+    const product = new ProductRecord(basicObject);
+
+    await product.insert();
+
+    expect(product.id).toBeDefined();
+})
 
